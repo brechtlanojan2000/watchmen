19df4cda636409fb960eb547868da8aa5742bfd9
Albrecht Lanojan <christian.lanojan1@gmail.com>
2020-10-11 21:24:57 +0800

    updated urls

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import Index, Login, Logout
+from .views import IndexView, LoginView, Logout
 
 urlpatterns = [
-    path('', Index.as_view(), name="index"),
-    path('login/', Login.as_view(), name="login"),
+    path('', IndexView.as_view(), name="index"),
+    path('login/', LoginView.as_view(), name="login"),
     path('logout/', Logout, name="logout")
 ]
