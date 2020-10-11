e9eabe5e0e4df7ae0f4bb667ac5321ea402af1b6
Albrecht Lanojan <christian.lanojan1@gmail.com>
2020-10-12 07:25:48 +0800

    mysql

--- config/settings.py
+++ config/settings.py
@@ -80,16 +80,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'NAME': 'watchmen',
-        # 'USER': 'root',
-        # 'PASSWORD': '',
-        # 'HOST': '',
-        # 'PORT': '',
-        # 'OPTIONS': {
-        #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        # }
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'watchmen',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
