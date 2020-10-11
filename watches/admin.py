bfafe5fb82e666ad7c025e91c9c9a729c2af40e4
Albrecht Lanojan <christian.lanojan1@gmail.com>
2020-10-11 21:23:50 +0800

    model register

--- watches/admin.py
+++ watches/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Brand, Watch, WatchImage
+
 # Register your models here.
+
+admin.site.register(Brand)
+admin.site.register(Watch)
+admin.site.register(WatchImage)
